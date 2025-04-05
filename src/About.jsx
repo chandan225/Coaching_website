b76c72f16a1c7e1af27ad054560f81182d5b1518
chandan kumar <chandan220302@gmail.com>
2025-04-05 11:53:40 +0530

    Update About.jsx
    corrected student spelling

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ export default function About()
     return(
         <div className="about m-3 bg-white/20 p-3 rounded-xl text-rose-50 ">
             <h2 className="text-2xl text-center">About us</h2>
-            <p className="p-2 ">We provide quality education for strudents from class 9th to 12th, providing education from last 5 years in Samastipur.</p>
+            <p className="p-2 ">We provide quality education for students from class 9th to 12th, providing education from last 5 years in Samastipur.</p>
         </div>
         
     );
